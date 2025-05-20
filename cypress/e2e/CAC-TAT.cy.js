77d872a75614ba0e598a8ebd9aa609cb76789e43
Jesiane <jesianef@hotmail.com>
2025-05-19 22:11:39 -0300

    Adicionanando informações no commands.js e removendo arquivos desnecessários.

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,7 +6,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
- it('Preencher os campos obrigatórios e enviar formulario.', () => {
+ it.only('Preencher os campos obrigatórios e enviar formulario.', () => {
     const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
     cy.get('#firstName').type('Jesi').should('have.value', 'Jesi')
     cy.get('#lastName').type('Freire').should('have.value', 'Freire')
@@ -42,8 +42,19 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('teste@teste.com').should('have.value', 'teste@teste.com').clear().should('have.value', '')
    cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('have.value','')
   })  
-  it.only('exibe mensagem de erro ao enviar o formulário sem preencher os campos obrigatórios', () => {
+  it('exibe mensagem de erro ao enviar o formulário sem preencher os campos obrigatórios', () => {
    cy.get('.button').click()
    cy.get('.error').should('contain', 'Valide os campos obrigatórios!').should('be.visible')  
   })
-})
+  it.only('Enviar formulário com sucesso usando um comando customizado', () => {
+    const data = {
+      firstName: 'Jesi',
+      lastName: 'Freire',
+      email: 'teste@teste.com',
+      message: 'Just a test.'
+    }
+    cy.fillMandatoryFieldsAndSubmit(data)
+    cy.get('.success').should('be.visible')
+    })
+    
+  })
